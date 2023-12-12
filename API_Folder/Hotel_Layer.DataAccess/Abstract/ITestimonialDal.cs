9fcb08bc8c112f06780fde875f4f32c8445dcf60
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-12 06:40:38 +0200

    GenericDal+Repo - <T>Interfaces - EF added.

---
+++ API_Folder/Hotel_Layer.DataAccess/Abstract/ITestimonialDal.cs
@@ -0,0 +1,13 @@
+﻿using Hotel_Layer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Layer.DataAccess.Abstract
+{
+    public interface ITestimonialDal : IGenericDal<Testimonial>
+    {
+    }
+}
---
+++ API_Folder/Hotel_Layer.DataAccess/EF/EfTestimonialDal.cs
@@ -0,0 +1,19 @@
+﻿using Hotel_Layer.DataAccess.Abstract;
+using Hotel_Layer.DataAccess.Concrete;
+using Hotel_Layer.DataAccess.Repositories;
+using Hotel_Layer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Layer.DataAccess.EF
+{
+    public class EfTestimonialDal : GenericRepository<Testimonial>, ITestimonialDal
+    {
+        public EfTestimonialDal(Context context) : base(context)
+        {
+        }
+    }
+}
