c95cc7bb3816cb6da3325590c6930544c4a3e4ba
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-14 08:47:31 +0200

    HttpClient method added for httpconfigure services.

--- Frontend/HotelProjectUI/Program.cs
+++ Frontend/HotelProjectUI/Program.cs
@@ -7,6 +7,7 @@ namespace HotelProjectUI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddHttpClient();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
