34b59426aaf789a6b07ad4fed797be3c804f855a
Umut Özbilek <umutozbilek@hotmail.com>
2023-12-17 02:02:01 +0300

    booking controller done

--- Frontend/HotelProjectUI/Controllers/BookingController.cs
+++ Frontend/HotelProjectUI/Controllers/BookingController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProjectUI.Dtos.BookingDto;
+using HotelProjectUI.Dtos.Subscriber;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProjectUI.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public PartialViewResult AddBooking()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task <IActionResult> AddBooking(CreateBookingDto createBookingDto)
+        {
+            createBookingDto.Status = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var dataJs = JsonConvert.SerializeObject(createBookingDto);
+            StringContent content = new StringContent(dataJs, Encoding.UTF8, "application/json");
+            await client.PostAsync("https://localhost:7120/api/Booking", content);
+            return RedirectToAction("Index", "Default");
         }
     }
 }
