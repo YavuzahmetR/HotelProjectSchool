f19a5fbe60a3cc34f5b14b20b41930f7d2f786e4
Umut Özbilek <umutozbilek@hotmail.com>
2023-12-17 00:10:10 +0300

    booking UI / controller added

---
+++ Frontend/HotelProjectUI/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProjectUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
