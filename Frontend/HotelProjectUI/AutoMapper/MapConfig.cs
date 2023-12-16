325626ee3f0d69097a8e9636c89580cfef4e0b20
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-16 06:08:53 +0200

    MapConfigs done.

--- Frontend/HotelProjectUI/AutoMapper/MapConfig.cs
+++ Frontend/HotelProjectUI/AutoMapper/MapConfig.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using Hotel_Layer.Entities.Concrete;
+using HotelProjectUI.Dtos.AboutDto;
 using HotelProjectUI.Dtos.AppUserDto;
 using HotelProjectUI.Dtos.LoginDto;
 using HotelProjectUI.Dtos.ServiceDto;
+using HotelProjectUI.Dtos.StaffDto;
+using HotelProjectUI.Dtos.Subscriber;
+using HotelProjectUI.Dtos.TestimonialDto;
 
 namespace HotelProjectUI.AutoMapper
 {
@@ -15,6 +19,11 @@ namespace HotelProjectUI.AutoMapper
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateUserDto, AppUser>().ReverseMap();
             CreateMap<SignInUserDto,AppUser>().ReverseMap();
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<ResultStaffDto, Staff>().ReverseMap();
+            CreateMap<CreateSubscriberDto, Subscriber>().ReverseMap();
         }
     }
 }
