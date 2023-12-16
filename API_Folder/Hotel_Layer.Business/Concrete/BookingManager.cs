f9fc4e7a7ce79daf6a2acb5aa67ef8bd7c41a4e2
Umut Özbilek <umutozbilek@hotmail.com>
2023-12-17 02:01:19 +0300

    Dal , Service , Manager (booking) done.

---
+++ API_Folder/Hotel_Layer.Business/Concrete/BookingManager.cs
@@ -0,0 +1,46 @@
+﻿using Hotel_Layer.Business.Abstract;
+using Hotel_Layer.DataAccess.Abstract;
+using Hotel_Layer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Layer.Business.Concrete
+{
+    public class BookingManager : IBookingService
+    {
+        private readonly IBookingDal _bookingDal;
+
+        public BookingManager(IBookingDal bookingDal)
+        {
+            _bookingDal = bookingDal;
+        }
+
+        public void TDelete(Booking t)
+        {
+            _bookingDal.Delete(t);
+        }
+
+        public Booking TGetById(int id)
+        {
+            return _bookingDal.GetById(id);
+        }
+
+        public List<Booking> TGetList()
+        {
+            return _bookingDal.GetList();
+        }
+
+        public void TInsert(Booking t)
+        {
+            _bookingDal.Insert(t);
+        }
+
+        public void TUpdate(Booking t)
+        {
+            _bookingDal.Update(t);
+        }
+    }
+}
