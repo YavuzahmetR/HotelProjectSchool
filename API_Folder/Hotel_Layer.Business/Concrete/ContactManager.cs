8220eebe6655fda8e575bcf52eb619d6c92b8a1d
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-18 23:49:36 +0200

    Context class added on db(API)

---
+++ API_Folder/Hotel_Layer.Business/Concrete/ContactManager.cs
@@ -0,0 +1,44 @@
+﻿using Hotel_Layer.Business.Abstract;
+using Hotel_Layer.DataAccess.Abstract;
+using Hotel_Layer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Layer.Business.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+        public void TDelete(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Contact t)
+        {
+            _contactDal.Insert(t);
+        }
+
+        public void TUpdate(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API_Folder/Hotel_Layer.DataAccess/EF/EfContactDal.cs
@@ -0,0 +1,17 @@
+﻿using Hotel_Layer.DataAccess.Abstract;
+using Hotel_Layer.DataAccess.Concrete;
+using Hotel_Layer.DataAccess.Repositories;
+using Hotel_Layer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Layer.DataAccess.EF
+{
+    public class EfContactDal : GenericRepository<Contact>, IContactDal
+    {
+        public EfContactDal(Context context):base(context) { }
+    }
+}
