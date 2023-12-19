bfa2141ae5023c44766a9af110defcbf1780a872
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-19 04:24:46 +0200

    About Controller for Admin side added.

---
+++ Frontend/HotelProjectUI/Controllers/AboutAdminController.cs
@@ -0,0 +1,59 @@
+﻿using HotelProjectUI.Dtos.AboutDto;
+using HotelProjectUI.Dtos.BookingDto;
+using HotelProjectUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProjectUI.Controllers
+{
+    public class AboutAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AboutAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7120/api/About");
+            if (response.IsSuccessStatusCode)
+            {
+                var dataJs = await response.Content.ReadAsStringAsync();
+                var modelObject = JsonConvert.DeserializeObject<List<ResultAboutDto>>(dataJs);
+                return View(modelObject);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7120/api/About/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var dataJs = await response.Content.ReadAsStringAsync();
+                var modelObject = JsonConvert.DeserializeObject<UpdateAboutDto>(dataJs);
+                return View(modelObject);
+
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto dtoModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var dataJs = JsonConvert.SerializeObject(dtoModel);
+            StringContent content = new StringContent(dataJs, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync($"https://localhost:7120/api/About", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
