d6c7be3f9fbcfb772a7e97d12183d07e5297c1a4
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-19 17:36:50 +0200

    Admin Controller(Guest), Dtos added.

---
+++ Frontend/HotelProjectUI/Controllers/GuestController.cs
@@ -0,0 +1,83 @@
+﻿using HotelProjectUI.Dtos.GuestDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProjectUI.Controllers
+{
+    public class GuestController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public GuestController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7120/api/Guest");
+            if(response.IsSuccessStatusCode)
+            {
+                var dataJs = await response.Content.ReadAsStringAsync();
+                var modelObject = JsonConvert.DeserializeObject<List<ResultGuestDto>>(dataJs);
+                return View(modelObject);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddGuest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGuest(CreateGuestDto guestDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var dataJs = JsonConvert.SerializeObject(guestDto);
+            StringContent content = new StringContent(dataJs,Encoding.UTF8,"application/json");
+            var response = await client.PostAsync("https://localhost:7120/api/Guest",content);
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteGuest(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7120/api/Guest/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateGuest(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7120/api/Guest/{id}");
+            if(response.IsSuccessStatusCode)
+            {
+                var dataJs = await response.Content.ReadAsStringAsync();
+                var modelObject = JsonConvert.DeserializeObject<UpdateGuestDto>(dataJs);
+                return View(modelObject);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateGuest(UpdateGuestDto guestDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var dataJs = JsonConvert.SerializeObject(guestDto);
+            StringContent content = new StringContent(dataJs,Encoding.UTF8,"application/json");
+            var response = await client.PutAsync("https://localhost:7120/api/Guest", content);
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Frontend/HotelProjectUI/Dtos/GuestDto/CreateGuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProjectUI.Dtos.GuestDto
+{
+    public class CreateGuestDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
