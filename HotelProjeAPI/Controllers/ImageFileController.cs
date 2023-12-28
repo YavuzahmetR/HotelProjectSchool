e8858be45e0a99bfa3ca1371a6db683f539240e2
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-28 10:28:10 +0200

    API controllers(Image-Text) created for consume.

---
+++ HotelProjeAPI/Controllers/ImageFileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProjeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ImageUpload([FromForm]IFormFile file)
+        {
+            //wwwroot içine görsel dosyası göndermek için. Unique isim.
+            var fileName = Guid.NewGuid()+Path.GetExtension(file.FileName);
+            var directory = Path.Combine(Directory.GetCurrentDirectory(),"images/" + fileName);
+            FileStream fileStream = new FileStream(directory,FileMode.Create);
+            await file.CopyToAsync(fileStream);
+            return Created("", file);
+
+        }
+
+    }
+}
---
+++ HotelProjeAPI/Controllers/TextFileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProjeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> TextUpload([FromForm] IFormFile file)
+        {
+            try
+            {
+                if (file != null && file.Length > 0)
+                {
+                    var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    var directory = Path.Combine(Directory.GetCurrentDirectory(), "files", fileName);
+
+                    using (var fileStream = new FileStream(directory, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                    var fileContent = await System.IO.File.ReadAllTextAsync(directory);
+                    return Ok(new { fileName, fileContent });
+                }
+                else
+                {
+                    return BadRequest("Dosya bulunamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Sunucu Hatası: {ex}");
+            }
+        }
+    }
+}
