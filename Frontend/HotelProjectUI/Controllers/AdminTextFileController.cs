73a0587b0a552a441b4b553fac3abd097649724e
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-28 10:25:00 +0200

    Text File controller added.

---
+++ Frontend/HotelProjectUI/Controllers/AdminTextFileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProjectUI.Controllers
+{
+    public class AdminTextFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile textFile)
+        {
+            var stream = new MemoryStream();
+            await textFile.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArray = new ByteArrayContent(bytes);
+            byteArray.Headers.ContentType = new MediaTypeHeaderValue(textFile.ContentType);
+            MultipartFormDataContent multipartFormData = new MultipartFormDataContent();
+            multipartFormData.Add(byteArray, "file", textFile.FileName);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("https://localhost:7120/api/TextFile", multipartFormData);
+            return View();
+        }
+    }
+}
