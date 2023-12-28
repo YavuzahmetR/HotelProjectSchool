701e174a138ebfcebb751079a53877333f6af3d8
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-28 05:09:45 +0200

    Mail Model created.

---
+++ Frontend/HotelProjectUI/Models/Mail/AdminMailVM.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProjectUI.Models.Mail
+{
+    public class AdminMailVM
+    {
+        public string Name { get; set; }
+        public string SenderEmail { get; set; }
+        public string RecevierMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
