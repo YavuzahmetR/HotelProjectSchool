82c30293c466916e418e117b8224c8b3a473b5d0
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-28 10:26:03 +0200

    Error Page controller added.

---
+++ Frontend/HotelProjectUI/Controllers/ErrorStatusPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProjectUI.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorStatusPageController : Controller
+    {
+        public IActionResult E404()
+        {
+            return View();
+        }
+    }
+}
