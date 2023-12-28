d1c611d8d7ff875199352b8fa98df96f03240ebf
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-28 10:24:31 +0200

    Image File controller added.

---
+++ Frontend/HotelProjectUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProjectUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile imageFile)
+        {
+            var stream = new MemoryStream();
+            await imageFile.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArray = new ByteArrayContent(bytes);
+            byteArray.Headers.ContentType = new MediaTypeHeaderValue(imageFile.ContentType);
+            MultipartFormDataContent multipartFormData = new MultipartFormDataContent();
+            multipartFormData.Add(byteArray, "file", imageFile.FileName);
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("https://localhost:7120/api/ImageFile", multipartFormData);
+            return View();
+        }
+
+    }
+}
