45a64a283dec3d76e3ce6ef9927bcc76f7bd2a64
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-29 17:59:39 +0200

    SendBox-Inbox consumed on AContactController

--- Frontend/HotelProjectUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProjectUI/Controllers/AdminContactController.cs
@@ -19,25 +19,38 @@ namespace HotelProjectUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var response = await client.GetAsync("https://localhost:7120/api/Contact");
+
+            var client2 = _httpClientFactory.CreateClient();
+            var response2 = await client2.GetAsync("https://localhost:7120/api/Contact/GetContactNumber");
+
+            var client3 = _httpClientFactory.CreateClient();
+            var response3 = await client2.GetAsync("https://localhost:7120/api/SendMessage/GetSendMessageNumber");
             if (response.IsSuccessStatusCode)
             {
                 var dataJs = await response.Content.ReadAsStringAsync();
                 var modelObject = JsonConvert.DeserializeObject<List<InboxContactDto>>(dataJs);
-                return View(modelObject);
 
+                var dataJs2 = await response2.Content.ReadAsStringAsync();
+
+                var dataJs3 = await response3.Content.ReadAsStringAsync();
+
+                ViewBag.ContactNumber = dataJs2;
+
+                ViewBag.SendMessageNumber = dataJs3;
+
+                return View(modelObject);
             }
             return View();
         }
         public async Task<IActionResult> Sendbox()
         {
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("https://localhost:7120/api/SendMessage");
+            var response = await client.GetAsync("https://localhost:7120/api/SendMessage");          
             if (response.IsSuccessStatusCode)
             {
                 var dataJs = await response.Content.ReadAsStringAsync();
-                var modelObject = JsonConvert.DeserializeObject<List<ResultSendboxDto>>(dataJs);
+                var modelObject = JsonConvert.DeserializeObject<List<ResultSendboxDto>>(dataJs);               
                 return View(modelObject);
-
             }
             return View();
         }
