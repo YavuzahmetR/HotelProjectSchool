9ebc072f405cb1cbfdb00f138ae6e321a4348a0e
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-12-30 16:11:14 +0200

    Dto's done for contact-msg.category

---
+++ Frontend/HotelProjectUI/Dtos/MessageCategoryDto/ResultMessageCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelProjectUI.Dtos.MessageCategoryDto
+{
+    public class ResultMessageCategoryDto
+    {
+        public int MessageCategoryID { get; set; }
+        public string MessageCategoryName { get; set; }
+    }
+}
