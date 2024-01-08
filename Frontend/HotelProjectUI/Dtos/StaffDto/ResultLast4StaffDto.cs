cd734a76f472561976a3ef695f6a6778cfd9b9ab
Umut Özbilek <umutozbilek@hotmail.com>
2024-01-08 18:16:27 +0300

    Booking - 6 / Staff - 4 Dtos added

---
+++ Frontend/HotelProjectUI/Dtos/StaffDto/ResultLast4StaffDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProjectUI.Dtos.StaffDto
+{
+    public class ResultLast4StaffDto
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public string StaffTitle { get; set; }
+        public string SocialMediaFirst { get; set; }
+        public string SocialMediaSecond { get; set; }
+        public string SocialMediaThird { get; set; }
+    }
+}
